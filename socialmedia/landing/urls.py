185eed467d5ef3e0df9d7d600ab46afe2989fa4a
Prakash <prakash8128154996@gmail.com>
2021-04-20 13:10:53 +0530

    second commit

--- socialmedia/landing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from landing.views import Index
-
-
-urlpatterns = [
-    path('', Index.as_view(),name='index'),
-]
--- socialmedia/social/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PostListView
-
-urlpatterns = [
-    path('', PostListView.as_view(), name='post-list'),
-]
